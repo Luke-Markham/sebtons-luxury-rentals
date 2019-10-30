4641917c2d049c023464d0d314e3ff3ac533cf4d
Luke Markham <lukeeliotmarkham2@hotmail.com>
2019-10-30 10:35:21 +0700

    Updates

--- precache-manifest.2b5580baa712fc4d186a17afd5012d56.js
+++ precache-manifest.2b5580baa712fc4d186a17afd5012d56.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4243f5d8508a80117437b8ef33e369ba",
+    "revision": "3a87d9b64655a2874d6fe5d0218d7732",
     "url": "/sebtons-luxury-rentals/index.html"
   },
   {
-    "revision": "9684fbb06f8137224365",
+    "revision": "2e259663c44cc45027b1",
     "url": "/sebtons-luxury-rentals/static/css/main.5b0d8cf6.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/sebtons-luxury-rentals/static/js/2.74fefb5a.chunk.js"
   },
   {
-    "revision": "9684fbb06f8137224365",
-    "url": "/sebtons-luxury-rentals/static/js/main.69655f02.chunk.js"
+    "revision": "2e259663c44cc45027b1",
+    "url": "/sebtons-luxury-rentals/static/js/main.8377d1ec.chunk.js"
   },
   {
     "revision": "b296959b5bf1bd503e12",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sebtons-luxury-rentals/precache-manifest.aa576d5408d87b063d5007e26d8957fc.js"
+  "/sebtons-luxury-rentals/precache-manifest.2b5580baa712fc4d186a17afd5012d56.js"
 );
 
 self.addEventListener('message', (event) => {
