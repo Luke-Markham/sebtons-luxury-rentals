e63f28658c714f8b7587ac3e9256352c72feacbf
Luke Markham <lukeeliotmarkham2@hotmail.com>
2019-10-31 08:21:11 +0700

    Updates

---
+++ precache-manifest.3600aa96bf6d9e401b03628851dcaba0.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6481c1b3c1824455a41656dbf55240c0",
+    "url": "/sebtons-luxury-rentals/index.html"
+  },
+  {
+    "revision": "32a70a97363e8e524661",
+    "url": "/sebtons-luxury-rentals/static/css/10.e8383354.chunk.css"
+  },
+  {
+    "revision": "5b35350c1ad3bab2b18c",
+    "url": "/sebtons-luxury-rentals/static/css/11.b25f0674.chunk.css"
+  },
+  {
+    "revision": "0fa1958f458293a1130c",
+    "url": "/sebtons-luxury-rentals/static/css/12.c80ce174.chunk.css"
+  },
+  {
+    "revision": "19de61be06b0892482f0",
+    "url": "/sebtons-luxury-rentals/static/css/5.02d7324d.chunk.css"
+  },
+  {
+    "revision": "c3c1dcb2fe33fa9f5fce",
+    "url": "/sebtons-luxury-rentals/static/css/6.61083ca3.chunk.css"
+  },
+  {
+    "revision": "59b8c702ff070d34a1a0",
+    "url": "/sebtons-luxury-rentals/static/css/7.d03f7009.chunk.css"
+  },
+  {
+    "revision": "ad3754d2d33acd50571c",
+    "url": "/sebtons-luxury-rentals/static/css/8.7e94712d.chunk.css"
+  },
+  {
+    "revision": "672519a2265ac4de0f66",
+    "url": "/sebtons-luxury-rentals/static/css/9.0c092bc6.chunk.css"
+  },
+  {
+    "revision": "0590b23b705d3f1f2847",
+    "url": "/sebtons-luxury-rentals/static/css/main.153961d2.chunk.css"
+  },
+  {
+    "revision": "b03db7af099c223779af",
+    "url": "/sebtons-luxury-rentals/static/js/0.72582021.chunk.js"
+  },
+  {
+    "revision": "f1ca39d81e931a38ff17",
+    "url": "/sebtons-luxury-rentals/static/js/1.a00d94f3.chunk.js"
+  },
+  {
+    "revision": "32a70a97363e8e524661",
+    "url": "/sebtons-luxury-rentals/static/js/10.361dd0cb.chunk.js"
+  },
+  {
+    "revision": "5b35350c1ad3bab2b18c",
+    "url": "/sebtons-luxury-rentals/static/js/11.291af47d.chunk.js"
+  },
+  {
+    "revision": "0fa1958f458293a1130c",
+    "url": "/sebtons-luxury-rentals/static/js/12.8cab9ddf.chunk.js"
+  },
+  {
+    "revision": "74e1f4290bdcdce08ff6",
+    "url": "/sebtons-luxury-rentals/static/js/4.159c1175.chunk.js"
+  },
+  {
+    "revision": "19de61be06b0892482f0",
+    "url": "/sebtons-luxury-rentals/static/js/5.0be6d773.chunk.js"
+  },
+  {
+    "revision": "c3c1dcb2fe33fa9f5fce",
+    "url": "/sebtons-luxury-rentals/static/js/6.336cba04.chunk.js"
+  },
+  {
+    "revision": "59b8c702ff070d34a1a0",
+    "url": "/sebtons-luxury-rentals/static/js/7.dcde13db.chunk.js"
+  },
+  {
+    "revision": "ad3754d2d33acd50571c",
+    "url": "/sebtons-luxury-rentals/static/js/8.1f218de4.chunk.js"
+  },
+  {
+    "revision": "672519a2265ac4de0f66",
+    "url": "/sebtons-luxury-rentals/static/js/9.53edace7.chunk.js"
+  },
+  {
+    "revision": "0590b23b705d3f1f2847",
+    "url": "/sebtons-luxury-rentals/static/js/main.276c37fe.chunk.js"
+  },
+  {
+    "revision": "d2ccb5c99680730c9278",
+    "url": "/sebtons-luxury-rentals/static/js/runtime-main.f8bc016b.js"
+  },
+  {
+    "revision": "29de04011d079aa0c6d108c0ff0d599a",
+    "url": "/sebtons-luxury-rentals/static/media/Lital17.29de0401.jpg"
+  },
+  {
+    "revision": "d36a8fb4b9b8997e262e5c344b449e22",
+    "url": "/sebtons-luxury-rentals/static/media/StLuciaFlag.d36a8fb4.jpeg"
+  },
+  {
+    "revision": "361be7945c8581a937b6125c841c253e",
+    "url": "/sebtons-luxury-rentals/static/media/about-us-bkg.361be794.jpg"
+  },
+  {
+    "revision": "9f6650f4f897d9dc6bd0039a3f03699a",
+    "url": "/sebtons-luxury-rentals/static/media/about-us-cards-bkg.9f6650f4.jpg"
+  },
+  {
+    "revision": "6468634d7642b247f4a6247b94d05553",
+    "url": "/sebtons-luxury-rentals/static/media/back-arrow.6468634d.svg"
+  },
+  {
+    "revision": "a26b7cf2965008ca0e4058168ac71ab0",
+    "url": "/sebtons-luxury-rentals/static/media/bathrooms.a26b7cf2.svg"
+  },
+  {
+    "revision": "a84a8976dcac192b8d82e05686833825",
+    "url": "/sebtons-luxury-rentals/static/media/bedrooms.a84a8976.svg"
+  },
+  {
+    "revision": "14ff14ecf3728b0810e0889f96b219fb",
+    "url": "/sebtons-luxury-rentals/static/media/boatRental-charters.14ff14ec.jpg"
+  },
+  {
+    "revision": "2f68b74bb9e48b3017aa52f213f366aa",
+    "url": "/sebtons-luxury-rentals/static/media/chauffeur.2f68b74b.jpg"
+  },
+  {
+    "revision": "ee4ab24e84891ff9243fc6823e04929a",
+    "url": "/sebtons-luxury-rentals/static/media/childcare.ee4ab24e.jpg"
+  },
+  {
+    "revision": "96bf4183455ab1fcb4b485dc4ce3f450",
+    "url": "/sebtons-luxury-rentals/static/media/closeNav.96bf4183.svg"
+  },
+  {
+    "revision": "7f7a3a14f8e768c194d9f47d2db3e677",
+    "url": "/sebtons-luxury-rentals/static/media/concierge.7f7a3a14.jpg"
+  },
+  {
+    "revision": "5583d99590b6f0775db1ebe5922c960d",
+    "url": "/sebtons-luxury-rentals/static/media/exclusivity.5583d995.svg"
+  },
+  {
+    "revision": "8dc1e6b0952c9639df00da8fbd67b013",
+    "url": "/sebtons-luxury-rentals/static/media/experience.8dc1e6b0.svg"
+  },
+  {
+    "revision": "af092d337cd12698f04530d292d25c14",
+    "url": "/sebtons-luxury-rentals/static/media/facebook.af092d33.svg"
+  },
+  {
+    "revision": "4976368b00370ae55b82400db95a2106",
+    "url": "/sebtons-luxury-rentals/static/media/housekeeping.4976368b.jpg"
+  },
+  {
+    "revision": "76b382d849116442d3792f1c6a071fae",
+    "url": "/sebtons-luxury-rentals/static/media/hygiene.76b382d8.jpg"
+  },
+  {
+    "revision": "348681045e80afa6254a4a289148ff10",
+    "url": "/sebtons-luxury-rentals/static/media/in-house-chef.34868104.jpg"
+  },
+  {
+    "revision": "05fc7fc0f2d70715575a1f36464ccf4f",
+    "url": "/sebtons-luxury-rentals/static/media/instagram.05fc7fc0.svg"
+  },
+  {
+    "revision": "f03c71ae25ec510a1de3ef553e9d1fc2",
+    "url": "/sebtons-luxury-rentals/static/media/introBkg.f03c71ae.jpeg"
+  },
+  {
+    "revision": "383ae630413406a0f12533bb15326b0c",
+    "url": "/sebtons-luxury-rentals/static/media/linen-towels.383ae630.jpg"
+  },
+  {
+    "revision": "4b0ff2f316d7dd9a40e222619ddddcd9",
+    "url": "/sebtons-luxury-rentals/static/media/location.4b0ff2f3.svg"
+  },
+  {
+    "revision": "f1b8a50fcbdf4a63afe044e9345aa301",
+    "url": "/sebtons-luxury-rentals/static/media/luxury-rental-car.f1b8a50f.jpg"
+  },
+  {
+    "revision": "730e2e1245dce38293e4eafcb72dd418",
+    "url": "/sebtons-luxury-rentals/static/media/next.730e2e12.svg"
+  },
+  {
+    "revision": "732fe4cc02d8993020130fc5ec363997",
+    "url": "/sebtons-luxury-rentals/static/media/nights.732fe4cc.svg"
+  },
+  {
+    "revision": "a8c1d3d51ade513bf38d39b1ac1001cd",
+    "url": "/sebtons-luxury-rentals/static/media/openNav.a8c1d3d5.svg"
+  },
+  {
+    "revision": "47be44e381898d0ea73310f64da486bc",
+    "url": "/sebtons-luxury-rentals/static/media/person.47be44e3.png"
+  },
+  {
+    "revision": "02eef2ab0385d7c685170abe13e50271",
+    "url": "/sebtons-luxury-rentals/static/media/personalization.02eef2ab.svg"
+  },
+  {
+    "revision": "a8f7f8cd72121312c67e15b103e71da3",
+    "url": "/sebtons-luxury-rentals/static/media/phone.a8f7f8cd.svg"
+  },
+  {
+    "revision": "2a814080223a53149d70562c64dc1131",
+    "url": "/sebtons-luxury-rentals/static/media/prev.2a814080.svg"
+  },
+  {
+    "revision": "d7adac28ad5a1f165610500f48450529",
+    "url": "/sebtons-luxury-rentals/static/media/price.d7adac28.svg"
+  },
+  {
+    "revision": "d81bbdc52c902d447ec1a42c2348d40d",
+    "url": "/sebtons-luxury-rentals/static/media/properties-bkg.d81bbdc5.jpg"
+  },
+  {
+    "revision": "a26b3f8e463cbc75674e9f9e6c619ac9",
+    "url": "/sebtons-luxury-rentals/static/media/scuba-diving.a26b3f8e.jpg"
+  },
+  {
+    "revision": "8ed2ade3b9100014a0339cb5c1536e31",
+    "url": "/sebtons-luxury-rentals/static/media/sebtons-logo-main.8ed2ade3.png"
+  },
+  {
+    "revision": "cc6733a0e5f072eae0e8cb0e190efb05",
+    "url": "/sebtons-luxury-rentals/static/media/services-bkg.cc6733a0.jpg"
+  },
+  {
+    "revision": "8f781c011dc98e77b5659b56b9b5875c",
+    "url": "/sebtons-luxury-rentals/static/media/spa-massage.8f781c01.jpg"
+  },
+  {
+    "revision": "df12f5d1e591a880f4d940ea786e9b32",
+    "url": "/sebtons-luxury-rentals/static/media/welcomeDrink.df12f5d1.jpg"
+  },
+  {
+    "revision": "8a3f1dac962d6309be6f264ac76bd600",
+    "url": "/sebtons-luxury-rentals/static/media/wifi.8a3f1dac.svg"
+  },
+  {
+    "revision": "17fc49cb35e75919210251e32e91216c",
+    "url": "/sebtons-luxury-rentals/static/media/worldmap.17fc49cb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sebtons-luxury-rentals/precache-manifest.b71b21fdbfa78f1fecab13302fba267c.js"
+  "/sebtons-luxury-rentals/precache-manifest.3600aa96bf6d9e401b03628851dcaba0.js"
 );
 
 self.addEventListener('message', (event) => {
